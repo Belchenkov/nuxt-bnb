8f2a22757ce4261fb66d1085221208c6bd325c28
Aleksey Belchenkov <u608110@gmail.com>
2021-06-23 11:53:08 +0400

    HomeCard component

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>NuxtBnB</h1>
+
+    <div :key="home.objectID" v-for="home in homes" style="float: left;margin: 10px">
+      <home-card :home="home"></home-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import homes from '~/data/homes.json';
+
+export default {
+  name: "index",
+  data() {
+    return {
+      homes: homes.slice(0,3)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
